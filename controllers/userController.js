7fdba5dc94299b09105455febc307c16fff77312
Ebune Daniel Yahaya <ebunedy@gmail.com>
2022-06-30 18:44:56 +0100

    first work on user model and controller

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
+//all error functions and roken function yet to be created
+const register = async (req, res) => {
+  const { firstName, lastName, email, phoneNumber, password, comfirmPassword } =
+    req.body;
+  if (
+    firstName ||
+    !lastName ||
+    !email ||
+    !phoneNumber ||
+    !password ||
+    !comfirmPassword
+  ) {
+    throw new BadRequestError("provide registration details");
+  }
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new BadRequestError("Email already exists");
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    password,
+    comfirmPassword,
+    role,
+  });
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+};
--- models/User.js
+++ models/User.js
@@ -1,3 +1,68 @@
-const mongoose = require('mongose');
+const mongoose = require("mongose");
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 const { model, Schema } = mongoose;
+
+const userSchema = Schema({
+  firstName: {
+    type: String,
+    required: [true, "please provide first name"],
+    minLength: 3,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, "please provide last name"],
+    minLength: 3,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "provide valid email",
+    },
+    unique: true,
+  },
+  phoneNubmer: {
+    type: String,
+    required: [true, "please provide phone number"],
+    validate: {
+      validator: validator.isMobilePhone,
+      message: "please provide a valid mobile number",
+    },
+  },
+  pasword: {
+    type: String,
+    minLength: 8,
+    required: [true, "please provide password"],
+  },
+  comfirmPasword: {
+    type: String,
+    minLength: 8,
+    required: [true, "please provide password"],
+  },
+  role: {
+    type: String,
+    required: [true, "please provide a role"],
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  if (this.pasword === this.comfirmPasword) {
+    this.password = await bcrypt.hash(this.password, salt);
+    this.comfirmPasword = await bcrypt.hash(this.comfirmPasword, salt);
+  }
+});
+
+userModel.methods.comparePasswords = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+module.exports = model("User", userSchema);
+
+//first name, last name, email, phone number, password, confirm password
