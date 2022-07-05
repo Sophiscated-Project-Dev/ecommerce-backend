6a975dabce6d75d128dd228dadb1b9997835c772
Ebune Daniel Yahaya <ebunedy@gmail.com>
2022-07-05 09:47:07 +0100

    product model midlleware fixed

--- models/Product.js
+++ models/Product.js
@@ -80,4 +80,8 @@ productSchema.virtual("reviews", {
   justOne: false,
 });
 
+productSchema.post("remove", async function () {
+  await this.model("Review").deleteMany({ product: this._id });
+});
+
 module.exports = model("Product", productSchema);
