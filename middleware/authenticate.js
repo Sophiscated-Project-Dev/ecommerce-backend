0d474164a7a447c37f6e8d03b56661659a7467c6
Ebune Daniel Yahaya <ebunedy@gmail.com>
2022-07-08 11:22:39 +0100

    authentication, product model, product routes creation

---
+++ middleware/authenticate.js
@@ -0,0 +1,36 @@
+const { decodeToken } = require("../utils");
+const { UnauthenticatedError, UnauthorizedError } = require("../errors/index");
+
+const authenticateUsser = async (req, res, next) => {
+  let token;
+  const cookieToken = req.signedCookies.token;
+  const authHeader = req.headers.authorization;
+
+  //check cookie for token
+  if (cookieToken) {
+    token = cookieToken;
+  } else if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  }
+  if (!token) {
+    throw new UnauthenticatedError("Invalid authentication");
+  }
+  try {
+    const decodedToken = decodeToken(token);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Invalid authentication");
+  }
+};
+
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError("Unauthourized. Access denied");
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUsser, authorizePermissions };
