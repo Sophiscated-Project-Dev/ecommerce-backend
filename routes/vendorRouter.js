8ede3f0fa7b81fb758bcdd9d3973eac9b1f5643e
Ebune Daniel Yahaya <ebunedy@gmail.com>
2022-07-19 17:25:30 +0100

    vendor added

--- routes/vendorRouter.js
+++ routes/vendorRouter.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 const { registerVendor } = require("../controllers/vendorController");
 
-router.route("/register", registerVendor);
+router.route("/register").post(registerVendor);
 
 module.exports = router;
