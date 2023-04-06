180f171d3695b485c3c6a5129d7bd0c18c928c00
ismat27 <adegbiteismail27@gmail.com>
2023-04-06 21:27:33 +0100

    modify schema to include customer address

--- models/Order.js
+++ models/Order.js
@@ -1,35 +1,38 @@
 const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema({
-  orderNumber: { type: String, required: true, unique: true },
-  customer: {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    zip: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  },
-  items: [
-    {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-      name: { type: String, required: true },
-      price: { type: Number, required: true },
-      quantity: { type: Number, required: true },
+const orderSchema = new mongoose.Schema(
+  {
+    orderNumber: { type: String, required: true, unique: true },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true
     },
-  ],
-  subtotal: { type: Number, required: true },
-  shippingFee: { type: Number, required: true },
-  tax: { type: Number, required: true },
-  total: { type: Number, required: true },
-  paymentMethod: { type: String, required: true },
-  paymentStatus: { type: String, enum: ['unpaid', 'paid'], default: 'unpaid' },
-  fulfillmentStatus: { type: String, enum: ['unfulfilled', 'fulfilled'], default: 'unfulfilled' },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
+    billingAddress: {
+      fullName: {type: String, required: true, trim: true},
+      email: { type: String, required: true, trim: true },
+      street: { type: String, required: true, trim: true },
+      city: { type: String, required: true, trim: true },
+      state: { type: String, required: true, trim: true },
+    },
+    shippingAddress: {
+      fullName: {type: String, required: true, trim: true},
+      email: { type: String, required: true, trim: true },
+      street: { type: String, required: true, trim: true },
+      city: { type: String, required: true, trim: true },
+      state: { type: String, required: true, trim: true },
+    },
+    items: [
+      { type: mongoose.Schema.Types.ObjectId, ref: 'OrderItem' }
+    ],
+    subtotal: { type: Number, required: [true, 'subtotal is required'] },
+    shippingFee: { type: Number, required: true, default: 0 },
+    tax: { type: Number, required: true, default: 0  },
+    total: { type: Number, required: [true, 'total amount due is required'] },
+    paymentMethod: { type: String, required: true, default: 'card' },
+    paymentStatus: { type: String, enum: ['unpaid', 'paid'], default: 'unpaid' },
+    fulfillmentStatus: { type: String, enum: ['unfulfilled', 'fulfilled'], default: 'unfulfilled' },
+  },
+  {timestamps: true}
+);
 
 const Order = mongoose.model('Order', orderSchema);
 
